2186cac17288330db9d891aa475f46385ac75cde
successbyfailure <successbyfailure@gmail.com>
2023-04-22 02:55:41 +0200

    Update next.config.js

--- site/next.config.js
+++ site/next.config.js
@@ -2,10 +2,10 @@
 
 const nextConfig = {
   env: {
-    name: 'Kasm Technologies',
-    description: 'The official store for Kasm supported workspaces.',
+    name: 'Mapache Webtops',
+    description: 'workspaces.',
     icon: '/img/logo.svg',
-    listUrl: 'https://registry.kasmweb.com/',
+    listUrl: 'https://docker.mapache.xyz/',
     contactUrl: 'https://kasmweb.com/support',
   },
   reactStrictMode: true,
